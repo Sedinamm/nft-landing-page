b0ab0d09ed189da756a00cc4c1cdbf5d8d7b9751
Sedinam <sedinam436@gmail.com>
2022-10-18 11:29:46 -0200

    added a buton and an image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from './assets';
+
 const App = () => {
   return (
-    <div>
-      <h1  className="text-3xl font-bold underline">
-      React Native NFT Showcase</h1>
-    </div>
+    <>
+      <SectionWrapper
+      title ="Your own store of Cool NFTs.
+      Start Sellings & Growning."
+      description = "Buy, Store, collect NFTs, exchange and earn crypto.Join 20+ Million people using ProNef Marketplace"
+
+      ShowBtn
+      mockupImg={assets.homeHero}
+      banner="banner"
+      />
+    </>
   );
-}
+};
 
 export default App;
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-const SectionWrapper = () => {
+import styles from '../styles/Global';
+import assets from '../assets';
+import Button from './Button';
+
+const SectionWrapper = ({ title, description,
+ShowBtn, mockupImg, banner }) => {
   return (
-    <div>SectionWrapper</div>
+    <div className={`min-h-screen ${styles.section}
+    ${styles.bgWhite} ${banner}`}>
+
+
+    <div className={`flex items-center ${styles.boxClass}`}>
+    <div className={`${styles.descDiv} fadeLeftMini`}>
+    <h1 className={`${styles.h1Text}`}>{title}</h1>
+    <p className={`${styles.descriptionText}`}>{description}</p>
+    {ShowBtn && (
+      <Button
+      assetsUrl={assets.expo}
+      link="deployed nft marketplace rn"
+      />
+    )}
+    </div>
+
+      <div
+      className={`flex-1 ${styles.flexCenter}
+      p-8 sm:px-0`}
+      >
+        <img src={mockupImg} alt="mockup"
+        className={styles.sectionImg}
+        />
+      </div>
+    </div>  
+    </div>
   )
 }
 
