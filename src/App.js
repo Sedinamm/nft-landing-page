f5f2d94cbb674693af179f90b1ad93e072d749f6
Sedinam <sedinam436@gmail.com>
2023-01-05 13:48:30 -0300

    changed some text title in App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
         reverse
       />
  <SectionWrapper 
-        title="Creative way to showcase the store"
+        title="Creative way to showcase your store"
         description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT"
         mockupImg={assets.mockup}
         banner="banner02"
