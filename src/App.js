d518e9b6db4eb84878d53e2d5453bed5455b3c6f
Sedinam <sedinam436@gmail.com>
2023-01-06 13:28:06 -0300

    footer text changed

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>Made by {" "}
-        <span className="bold">Gabriel Sedinam</span>
+        <span className="bold">Sedinam</span>
         </p>
       </div>
     </>
