383a3a8328d9116945e19b1c45b0257ed3988740
Sedinam <sedinam436@gmail.com>
2023-01-06 13:26:05 -0300

    text correction

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
             <SectionWrapper 
-        title="You own store of Nifty NFTs. Start Selling & Growing"
+        title="Your own store of amaizing NFTs. Start Selling & Growing"
         description="Buy, store, collect NFTS, exchange & earn crypto. Join 30+ million people using NFTbiz Marketplace."
         showBtn
         mockupImg={assets.homeHero}
